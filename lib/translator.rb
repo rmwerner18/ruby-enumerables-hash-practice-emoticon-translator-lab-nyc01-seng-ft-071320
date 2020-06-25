35fdbb8fca8310ebecc42b2eb58ab6b7d2358938
Ryan Werner <rmwerner@umich.edu>
2020-06-25 19:43:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,6 +9,7 @@ def load_library(file)
     emoticons[keys[count]] = {english: emoticons[keys[count]][0], japanese: emoticons[keys[count]][1]}
     count += 1
   end
+  emoticons
 end
 
 def get_japanese_emoticon
