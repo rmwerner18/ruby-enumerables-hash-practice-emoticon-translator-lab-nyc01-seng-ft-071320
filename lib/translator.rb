290e3f21c3a9d35454ff45e4e92ab763380294db
Ryan Werner <rmwerner@umich.edu>
2020-06-25 20:08:19 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,39 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file)
+  emoticons = YAML.load_file(file)
+  keys = emoticons.keys
+  count = 0
+  while count < keys.length do
+    emoticons[keys[count]] = {english: emoticons[keys[count]][0], japanese: emoticons[keys[count]][1]}
+    count += 1
+  end
+  emoticons
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file, emoticon)
+  emoticons = load_library(file)
+  keys = emoticons.keys
+  count = 0
+  while count < keys.length do
+    if emoticons[keys[count]][:english] == emoticon
+      return emoticons[keys[count]][:japanese]
+    end
+    count += 1
+  end
+  return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file, emoticon)
+  emoticons = load_library(file)
+  keys = emoticons.keys
+  count = 0
+  while count < keys.length do
+    if emoticons[keys[count]][:japanese] == emoticon
+      return keys[count]
+    end
+    count += 1
+  end
+  return "Sorry, that emoticon was not found"
 end
