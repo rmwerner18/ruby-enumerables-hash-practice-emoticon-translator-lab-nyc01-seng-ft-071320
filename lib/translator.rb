91081eb62b290cf11fcefdf67baa89f32917a688
Ryan Werner <rmwerner@umich.edu>
2020-06-25 19:40:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,6 +5,10 @@ def load_library(file)
   emoticons = YAML.load_file(file)
   keys = emoticons.keys
   binding.pry
+  count = 0
+  while count < keys.length do
+    emoticons[keys[count]] = {english: emoticons[keys[count]][0], emoticons[keys[count]][1]}
+  end
 end
 
 def get_japanese_emoticon
